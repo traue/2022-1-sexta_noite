c5b2926d8aad9c5272ceec3bd4f93c6f9240615c
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-02-18 22:00:45 -0300

    Projeto de revisão Java

--- RevisaoJava/src/br/uninove/poo/Pessoa.java
+++ RevisaoJava/src/br/uninove/poo/Pessoa.java
@@ -4,18 +4,78 @@ package br.uninove.poo;
 public class Pessoa {
 
     //atributos (características)
-    String nome;
-    String cpf;
-    int idade;
-    char sexo;
+    private String nome;
+    private String cpf;
+    private int idade;
+    private char sexo;
+
+    //método construtor:
+    public Pessoa() {
+        nome = "";
+        cpf = "";
+        idade = 0;
+        sexo = 'I';
+    }
+
+    //sobrecarregar o construtor
+    //Assinatura do método: String String, int char
+    public Pessoa(String nome, String cpf, int idade, char sexo) {
+        //o "this" refere-se ao atributo local da classe (definido "lá em cima")
+        //a variável sem o "this" em frente, refere-se à veriável recebida via parâmetro no método
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
 
     //métodos (comportamento)
     void fazerAniversario() {
-        idade++; //é o mesmo que: idade = idade + 1;
+        setIdade(getIdade() + 1); //é o mesmo que: idade = idade + 1;
         System.out.println("Ôba festinha do balacobaco!");
     }
 
     void nascer() {
-        idade = 0;
+        setIdade(0);
     }
+
+    //encapsulamento:
+    //1º para cada atributo transformá-los em privado (private)
+    //2º criar um set para o atributo
+    //3º criar um get para o atriburo
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        if (idade < 0 || idade > 150) { //por algun motivo definimos essa regra de negócio
+            this.idade = -999;
+        } else {
+            this.idade = idade;
+        }
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
 }
--- RevisaoJava/src/br/uninove/poo/Principal.java
+++ RevisaoJava/src/br/uninove/poo/Principal.java
@@ -1,7 +1,31 @@
 package br.uninove.poo;
 
 public class Principal {
+
     public static void main(String[] args) {
-        
+        Pessoa p = new Pessoa();
+
+        //p.nome = "Thiago Traue";
+        //p.idade = 34;
+        //p.cpf = "123.123.123-00";
+        //p.sexo = 'M';
+        //-------------------------
+        //System.out.println("Olá mundo!!!");
+        //System.out.println("Olá " + p.nome + " seja bem-vindo(a)!");
+        //System.out.println("Ah! legal, você tem " + p.idade + " anos!");
+        p.setNome("Thiago G. Traue");
+        p.setCpf("123.123.123-00");
+        p.setIdade(200);
+        p.setSexo('M');
+
+        System.out.println("Nome: " + p.getNome());
+        System.out.println("Idade: " + p.getIdade());
+        System.out.println("CPF: " + p.getCpf());
+        System.out.println("Sexo: " + p.getSexo());
+
+        p.fazerAniversario();
+
+        System.out.println("Agora você tem " + p.getIdade() + " anos!");
+
     }
 }
